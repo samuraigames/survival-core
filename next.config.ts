8671cf90fddd1b4168a61b7097d00fa384645389
mostafa babahamou <mostafababahamou@gmail.com>
2025-08-30 05:34:24 +0000

    Did you mean firebase.studio? The site you just tried to visit looks fak

--- next.config.ts
+++ next.config.ts
@@ -24,11 +24,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: [
-      "https://*.cloudworkstations.dev",
-    ]
-  }
 };
 
 export default nextConfig;
