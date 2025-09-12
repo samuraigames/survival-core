5ba9fe708b0e4c8e7d36571a8986a284482d7af9
mostafa babahamou <mostafababahamou@gmail.com>
2025-09-12 05:27:37 +0000

    no scrolling

--- src/components/starfield.tsx
+++ src/components/starfield.tsx
@@ -3,7 +3,7 @@
 const Starfield = () => {
   return (
     <div
-      className="absolute inset-0 w-full h-full z-0 opacity-30 bg-repeat animate-[stars_200s_linear_infinite]"
+      className="absolute inset-0 w-full h-full z-0 opacity-30 bg-repeat scrolling-stars"
       style={{
         backgroundImage: "url('https://www.transparenttextures.com/patterns/stardust.png')",
       }}
