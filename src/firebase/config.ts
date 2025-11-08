e1d7a931198b44ce74352829cfe7fd050fc98a0c
mostafa babahamou <mostafababahamou@gmail.com>
2025-11-08 11:29:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "survival-core",
+  "appId": "1:749061747910:web:0e94e4fd49bfa443d313c7",
+  "apiKey": "AIzaSyARP3qCs_QZQCbnuEWYl612aNnhCEPafbI",
+  "authDomain": "survival-core.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "749061747910"
+};
